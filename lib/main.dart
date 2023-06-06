d503f044f2fcabf9c91465ec5e49cc09c878956e
jeanjerry <C109154229@nkust.edu.tw>
2023-06-07 04:11:01 +0800

    'local_notice'

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ import 'flutter_flow/internationalization.dart';
 import 'flutter_flow/nav/nav.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:http/http.dart' as http;
 List<CameraDescription> cameras = [];
-
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
@@ -22,6 +24,19 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+      callbackDispatcher,
+      isInDebugMode: false
+  );
+  Workmanager().registerPeriodicTask(
+    "1",
+    "simplePeriodicTask",
+    frequency: Duration(minutes: 15),
+  );
+
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await FlutterFlowTheme.initialize();
@@ -60,6 +75,53 @@ void main() async {
     child: MyApp(),
   ));
 }
+
+void callbackDispatcher() {                        //初始化 WorkManager 插件  註冊事件
+  Workmanager().executeTask((task, inputData) async {
+    // Check if the current time is between 10:00 AM and 10:59 AM
+    var now = DateTime.now();
+    if (now.hour >= 2 && now.hour < 24) {
+      FlutterLocalNotificationsPlugin flip = FlutterLocalNotificationsPlugin();
+      var android = AndroidInitializationSettings('@mipmap/ic_launcher');
+      var iOS = DarwinInitializationSettings();
+      var settings = InitializationSettings(android: android, iOS: iOS);
+      await flip.initialize(settings);
+      await _showNotificationWithDefaultSound(flip);
+    }
+    return Future.value(true);
+  });
+}
+Future _showNotificationWithDefaultSound(flip) async {
+
+// Show a notification after every 15 minute with the first   第一個通知每 15 分鐘顯示一次
+// appearance happening a minute after invoking the method   調用該方法後一分鐘出現的外觀
+  var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      //'your channel description',
+      importance: Importance.max,
+      priority: Priority.high
+  );
+  var iOSPlatformChannelSpecifics = new DarwinNotificationDetails();
+
+// initialise channel platform for both Android and iOS device.
+  var platformChannelSpecifics = new NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics
+  );
+  await flip.show(0, '復健通知', '請記得做復健', platformChannelSpecifics, payload: 'Default_Sound'
+  );
+    DateTime dateTime = DateTime.now();
+    var url = Uri.parse("http://140.127.114.38:10073/flutterphp/local_notifications.php");
+    await http.post(url,body: {
+      "notice": "請記得做復健",
+      "time":dateTime.toString(),
+      "account" : "airehab_01",
+    });
+    print("已寄送");
+    print(dateTime.toString());
+}
+
 var ip =('http://140.127.114.38:10073/flutterphp/');
 //var ip =('http://192.168.56.1/');
 class MyApp extends StatefulWidget {
@@ -96,6 +158,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    print(FFAppState().accountnumber);
     return ScreenUtilInit(
         designSize: const Size(360, 780),
         minTextAdapt: true,
